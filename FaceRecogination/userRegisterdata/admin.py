38d3f694f2c35125b253278d62dca82938e3a27d
kashishsinghyadav <kashishhsinghhh@gmail.com>
2024-02-21 20:56:30 +0530

    created usersregister data models

--- FaceRecogination/userRegisterdata/admin.py
+++ FaceRecogination/userRegisterdata/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from userRegisterdata.models import UserImage, UserRegister
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'user_mail', 'user_phone')
+
+admin.site.register(UserRegister, UserAdmin)
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'image')
+
+admin.site.register(UserImage, ImageAdmin)
+
 
 # Register your models here.
--- FaceRecogination/userRegisterdata/models.py
+++ FaceRecogination/userRegisterdata/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class UserRegister(models.Model):
+    user_name = models.CharField(max_length=50)
+    user_mail = models.EmailField()
+    user_phone = models.CharField(max_length=12)
+    
+    def __str__(self):
+        
+        return self.user_mail
+    
+class UserImage(models.Model):
+    user = models.ForeignKey(UserRegister, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+
+
+
 # Create your models here.
