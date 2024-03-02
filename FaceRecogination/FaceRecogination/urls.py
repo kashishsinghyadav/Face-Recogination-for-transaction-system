c8a132f290e48c84952528ffe4655e810d3efef2
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-03-03 01:30:16 +0530

    Update urls.py

--- FaceRecogination/FaceRecogination/urls.py
+++ FaceRecogination/FaceRecogination/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .models import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',register,name='register'),
+    path('login/',loginuser,name='login')
 ]
